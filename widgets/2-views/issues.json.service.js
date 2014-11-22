ffde33d32a16957dc46965bbb328c2690e885c67
Christoph Dorn <christoph@christophdorn.com>
2014-11-21 19:42:07 -0700

    issues by tag

--- widgets/2-views/issues.json.service.js
+++ widgets/2-views/issues.json.service.js
@@ -5,30 +5,79 @@ const PATH = require("path");
 const DB_NAME = "devcomp";
 
 exports.app = function(req, res, next) {
-	return res.r.tableEnsure(DB_NAME, "myissues", "issues", {
-		indexes: [
-			"updatedOn"
-		]
-	}, function(err, issuesTable) {
-        if (err) return next(err);
 
-		return issuesTable.orderBy({
-    		index: res.r.desc("updatedOn")
-    	}).filter({
-    	}).limit(100).run(res.r.conn, function(err, cursor) {
-    		if (err) return next(err);
-    		function respond(body) {
-				res.writeHead(200, {
-					"Content-Type": "application/json",
-					"Content-Length": body.length,
-	                "Cache-Control": "max-age=60"  // seconds
+	if (req.query.tag) {
+
+		var tag = JSON.parse(req.query.tag);
+
+		return res.r.tableEnsure(DB_NAME, "myissues", "issues", {
+			indexes: [
+				"updatedOn"
+			]
+		}, function(err, issuesTable) {
+	        if (err) return next(err);
+
+			return res.r.tableEnsure(DB_NAME, "myissues", "tags", {
+				indexes: [
+					"issue",
+					"name"
+				]
+			}, function(err, tagsTable) {
+		        if (err) return next(err);
+
+		        // TODO: Don't return tag record; just issue record.
+				return tagsTable.eqJoin("issue", issuesTable).filter({
+					left: {
+						name: tag.name,
+						value: tag.value
+					}
+				}).run(res.r.conn, function(err, cursor) {
+		    		if (err) return next(err);
+		    		function respond(body) {
+						res.writeHead(200, {
+							"Content-Type": "application/json",
+//							"Content-Length": body.length,
+			                "Cache-Control": "max-age=60"  // seconds
+						});
+					    return res.end(body);
+		    		}
+					return cursor.toArray(function(err, issues) {
+					    if (err) return next(err);
+						return respond(JSON.stringify(issues.map(function (record) {
+							return record.right;
+						}), null, 4));
+					});
 				});
-			    return res.end(body);
-    		}
-			return cursor.toArray(function(err, issues) {
-			    if (err) return next(err);
-				return respond(JSON.stringify(issues, null, 4));
 			});
-		});
-    });
+	    });
+
+	} else {
+
+		return res.r.tableEnsure(DB_NAME, "myissues", "issues", {
+			indexes: [
+				"updatedOn"
+			]
+		}, function(err, issuesTable) {
+	        if (err) return next(err);
+
+			return issuesTable.orderBy({
+	    		index: res.r.desc("updatedOn")
+	    	}).filter({
+	    	}).limit(100).run(res.r.conn, function(err, cursor) {
+	    		if (err) return next(err);
+	    		function respond(body) {
+					res.writeHead(200, {
+						"Content-Type": "application/json",
+//						"Content-Length": body.length,
+		                "Cache-Control": "max-age=60"  // seconds
+					});
+				    return res.end(body);
+	    		}
+				return cursor.toArray(function(err, issues) {
+				    if (err) return next(err);
+					return respond(JSON.stringify(issues, null, 4));
+				});
+			});
+	    });
+	}
 }
--- widgets/2-views/issues/issues.js
+++ widgets/2-views/issues/issues.js
@@ -18,7 +18,17 @@ define(function() {
 						return self.setHTM(_htm, {
 							"credentialsConfig": JSON.stringify({
 							    "uri": "http://myissues-collector-github." + window.API.config.hostname + ":8013/ensure/credentials"
-							})
+							}),
+							apis: {
+								"issues-all": {
+									"url": self.config.serviceBaseUri + "/io.devcomp.portal/2-views/issues.json",
+									"$label": self.config.serviceBaseUri + "/io.devcomp.portal/2-views/issues.json"
+								},
+								"issues-by-tag": {
+									"url": self.config.serviceBaseUri + "/io.devcomp.portal/2-views/issues.json?tag=" + encodeURIComponent(JSON.stringify({name:"fixed",value:"firebug-3.0.0-alpha.3"})),
+									"$label": self.config.serviceBaseUri + "/io.devcomp.portal/2-views/issues.json?tag=' + encodeURIComponent(JSON.stringify({name:\"fixed\",value:\"firebug-3.0.0-alpha.3\"}))"
+								}
+							}
 						}).then(function(tag) {
 							$("#button-sync-now").click(function () {
 								$.ajax({
