b88230169200b15a03a07aeb45ea9e257e7cb291
Christoph Dorn <christoph@christophdorn.com>
2014-11-24 21:22:22 -0800

    issues by tags

--- widgets/2-views/issues.json.service.js
+++ widgets/2-views/issues.json.service.js
@@ -5,23 +5,87 @@ const PATH = require("path");
 const DB_NAME = "devcomp";
 
 exports.app = function(req, res, next) {
-	return res.r.tableEnsure(DB_NAME, "myissues", "issues", {
-		indexes: [
-			"updatedOn"
-		]
-	}, function(err, issuesTable) {
-        if (err) return next(err);
 
-		return issuesTable.orderBy({
-    		index: res.r.desc("updatedOn")
-    	}).filter({
-    	}).limit(100).run(res.r.conn, function(err, cursor) {
-    		if (err) return next(err);
-    		function respond(body) {
-				res.writeHead(200, {
-					"Content-Type": "application/json",
-					"Content-Length": body.length,
-	                "Cache-Control": "max-age=60"  // seconds
+	if (req.query.tag) {
+
+		var tag = req.query.tag.split(";");
+
+		return res.r.tableEnsure(DB_NAME, "myissues", "issues", {
+			indexes: [
+				"updatedOn"
+			]
+		}, function(err, issuesTable) {
+	        if (err) return next(err);
+
+			return res.r.tableEnsure(DB_NAME, "myissues", "tags", {
+				indexes: [
+					"issue",
+					"name"
+				]
+			}, function(err, tagsTable) {
+		        if (err) return next(err);
+
+		        // TODO: Don't return tag record; just issue record.
+		        var filter = {
+					name: tag[0]		        	
+		        };
+		        if (tag.length === 2) {
+		        	filter.value = tag[1];
+		        }
+				return tagsTable.eqJoin("issue", issuesTable).filter({
+					left: filter
+				}).run(res.r.conn, function(err, cursor) {
+		    		if (err) return next(err);
+		    		function respond(body) {
+						res.writeHead(200, {
+							"Content-Type": "application/json",
+//							"Content-Length": body.length,
+			                "Cache-Control": "max-age=60"  // seconds
+						});
+					    return res.end(body);
+		    		}
+					return cursor.toArray(function(err, issues) {
+					    if (err) return next(err);
+						return respond(JSON.stringify(issues.map(function (record) {
+							var data = record.right;
+							data._filter = {
+								tag: {
+									name: record.left.name,
+									value: record.left.value
+								}
+							};
+							return data;
+						}), null, 4));
+					});
+				});
+			});
+	    });
+
+	} else {
+
+		return res.r.tableEnsure(DB_NAME, "myissues", "issues", {
+			indexes: [
+				"updatedOn"
+			]
+		}, function(err, issuesTable) {
+	        if (err) return next(err);
+
+			return issuesTable.orderBy({
+	    		index: res.r.desc("updatedOn")
+	    	}).filter({
+	    	}).limit(100).run(res.r.conn, function(err, cursor) {
+	    		if (err) return next(err);
+	    		function respond(body) {
+					res.writeHead(200, {
+						"Content-Type": "application/json",
+//						"Content-Length": body.length,
+		                "Cache-Control": "max-age=60"  // seconds
+					});
+				    return res.end(body);
+	    		}
+				return cursor.toArray(function(err, issues) {
+				    if (err) return next(err);
+					return respond(JSON.stringify(issues, null, 4));
 				});
 			    return res.end(body);
     		}
